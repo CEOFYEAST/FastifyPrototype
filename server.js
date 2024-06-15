6f1103c4de89d62b0c49b2312aa472db8cc2e2a9
CEOFYEAST <benton.diebold@me.com>
2024-06-15 10:37:22 -0700

    Added basic route, server startup capabilities and test file for http requests

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 const fastify = require('fastify')({logger: true})
+const PORT = 5000
 
+fastify.get('/items', (req, reply) => {
+    reply.send({test: 'Hello' })
+})
+
+const start = async () => {
+    try {
+        await fastify.listen({ port: PORT })
+    } catch(error) {
+        fastify.log.error(error)
+        process.exit(1)
+    }
+}
+
+start()
