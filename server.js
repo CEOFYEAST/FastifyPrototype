69e256b3d4e053b77198ab1e8af4b8ae19d5536d
CEOFYEAST <benton.diebold@me.com>
2024-06-19 15:53:59 -0700

    added fastify swagger

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const fastify = require('fastify')({logger: true})
+fastify.register(require('fastify-swagger', {
+    exposeRoute: true,
+    routePrefix: '/docs',
+    swagger: {
+        info: { title: 'fastify-api' }
+    }
+}))
 fastify.register(require('./routes/items'))
 
 const PORT = 5000
